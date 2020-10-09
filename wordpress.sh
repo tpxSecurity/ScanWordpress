a4efc350f46896f1e9e4b0a2ef6063b932a4b1b5
CreadPag <creadpag@gmail.com>
2020-10-09 11:59:04 -0500

    Add files via upload

---
+++ wordpress.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+echo -e "
+ ██████╗██████╗ ███████╗ █████╗ ██████╗ ██████╗  █████╗  ██████╗ 
+██╔════╝██╔══██╗██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗██╔════╝ 
+██║     ██████╔╝█████╗  ███████║██║  ██║██████╔╝███████║██║  ███╗
+██║     ██╔══██╗██╔══╝  ██╔══██║██║  ██║██╔═══╝ ██╔══██║██║   ██║
+╚██████╗██║  ██║███████╗██║  ██║██████╔╝██║     ██║  ██║╚██████╔╝
+ ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚═════╝ ╚═╝     ╚═╝  ╚═╝ ╚═════╝ 
+                                                                 "
+echo -e "
+\033[31m#######################################################\033[m
+          example: bash creadpag.sh https://creadpag.com
+\033[31m#######################################################\033[m"                             
+url="$1"
+newestwp=$(curl -sSL http://api.wordpress.org/core/version-check/1.7 | jq -r .offers[0].current)
+runningwp=$(curl -s -N $url | grep 'wp-embed.min.js?ver=' | sed 's:.*=::' | cut -c1-5)
+
+if [ "$newestwp" == "$runningwp" ] ; then
+  printf "%s is running Wordpress %s. Which is the newst version.\n" "$url" "$newestwp"
+else
+  printf "%s is out of date. You are running %s and %s is the latest version.\n" "$url" "$runningwp" "$newestwp"
+fi
+
+exit 1
+;;
